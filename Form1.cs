bfd3e54f86aefdfc2c9c12af2dd0728b2e92ec01
Игнат Репин <ignat-repin@mail.ru>
2020-05-04 17:46:17 +0300

    test

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esoft_Project
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenClients_Click(object sender, EventArgs e)
+        {
+            Form formClient = new FormClient();
+            formClient.Show();
+        }
+
+        private void ButtonOpenRealEstates_Click(object sender, EventArgs e)
+        {
+            Form FormRealEstate = new FormRealEstate();
+            FormRealEstate.Show();
+        }
+    }
+}
